da94bf6d909d1e6b254470e86c6e141efa46de5b
hyeon12 <seojh912_@naver.com>
2024-04-18 10:34:02 +0900

    싱글톤 패턴

--- 4. 프로그래밍 언어활용/source/day03/src/exam02/Student/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day03/src/exam02/Student/Ex02.java
@@ -3,7 +3,7 @@ package exam02.Student;
 public class Ex02 {
     public static void main(String[] args) {
         Student.id = 10;
-        System.out.println(Student.id);
+        System.out.println(Student.id); //객체와 상관없이 쓸 수 있는 변수(static 키워드)
 
         Student.staticMethod();
     }
