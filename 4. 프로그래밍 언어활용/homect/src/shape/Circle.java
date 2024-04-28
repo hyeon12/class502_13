b736874ddce1b2916da1816e130767cc9c9ebb55
hyeon12 <seojh912_@naver.com>
2024-04-28 18:32:45 +0900

    상속

---
+++ 4. 프로그래밍 언어활용/homect/src/shape/Circle.java
@@ -0,0 +1,15 @@
+package shape;
+
+public class Circle extends Shape{
+
+    public void draw(){
+        System.out.println("Circle");
+    }
+
+    /*
+    @Override
+    public void draw() {
+        super.draw();
+    }
+     */
+}
