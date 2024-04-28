ba8f81452576e4d0acf050d7ec28a146b07cc1da
hyeon12 <seojh912_@naver.com>
2024-04-29 08:53:45 +0900

    지네릭스 사용하는 이유

--- 4. 프로그래밍 언어활용/source/day10/src/exam05/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day10/src/exam05/Ex01.java
@@ -6,7 +6,7 @@ public class Ex01 {
         appleBox.setItem(new Apple()); // 타입 명시함으로써 타입 안정성 : 실행하기 전 오류 발생!!
         //appleBox.setItem(new Grape()); -> 오류
 
-        Apple apple = appleBox.getItem(); //형변환의 번거로움 X
+        Grape apple = appleBox.getItem(); //형변환의 번거로움 X
         System.out.println(apple.get());
 
         Box <Grape> grapeBox = new Box<Grape>();
