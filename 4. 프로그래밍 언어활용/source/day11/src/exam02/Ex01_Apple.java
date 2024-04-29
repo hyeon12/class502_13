6b940bbd075210966dec2fcf5c737e99d2326dc8
hyeon12 <seojh912_@naver.com>
2024-04-29 14:39:54 +0900

    지네릭 클래스, 지네릭 메서드

--- 4. 프로그래밍 언어활용/source/day11/src/exam02/Ex01_Apple.java
+++ 4. 프로그래밍 언어활용/source/day11/src/exam02/Ex01_Apple.java
@@ -6,6 +6,6 @@ public class Ex01_Apple {
         appleBox.add(new Apple()); //add() 사과를 박스에 담았다!
         appleBox.add(new Apple()); //사과를 한개 더 담자!
 
-        Juicer.make(appleBox); // make() 주스를 만들자!
+        Juicer.make3(appleBox); // make() 주스를 만들자!
     }
 }
--- 4. 프로그래밍 언어활용/source/day11/src/exam03/Box.java
+++ 4. 프로그래밍 언어활용/source/day11/src/exam03/Box.java
@@ -4,12 +4,12 @@ public class Box <T>{
 
     private T item;
 
-    //Box가 객체가 될 때 T의 자료형이 결정 -> 지네릭 클래스의 T
+    //Box가 객체가 될 때 T의 자료형이 결정 - 지네릭 클래스의 T
     public void method1(T str1, T str2) {
 
     }
 
-    //지네릭메서드 - 함수 호출 시에 T의 자료형 결정 -> 지네릭 메서드의 T
+    //지네릭메서드 - 함수 호출 시에 T의 자료형 결정 - 지네릭 메서드의 T
     public <T> void method2(T str1, T str2){
 
     }
