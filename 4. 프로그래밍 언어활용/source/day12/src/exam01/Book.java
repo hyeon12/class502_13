3e778d07409ea9937bab5daa163de7bc2d84338a
hyeon12 <seojh912_@naver.com>
2024-04-30 14:02:01 +0900

    정렬

--- 4. 프로그래밍 언어활용/source/day12/src/exam01/Book.java
+++ 4. 프로그래밍 언어활용/source/day12/src/exam01/Book.java
@@ -1,6 +1,8 @@
 package exam01;
 
-public class Book {
+import java.util.Objects;
+
+public class Book implements Comparable<Book> {
     private int isbn;
     private String title;
     private String author;
@@ -12,11 +14,34 @@ public class Book {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Book){
+            Book book = (Book)obj;
+            if(isbn == book.isbn && title.equals(book.title) && author.equals(book.author)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn, title, author);
+    }
+
+
+    @Override
     public String toString() {
         return "Book{" +
                 "isbn=" + isbn +
                 ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return isbn - o.isbn; // 오름차순
+        //return o.isbn - isbn; // 내림차순
     }
 }
