e351ad311f40c86cc21b26bd54cb9832c92a1bc7
hyeon12 <seojh912_@naver.com>
2024-05-02 17:00:29 +0900

    람다식

--- 4. 프로그래밍 언어활용/source/day14/src/exam01/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day14/src/exam01/Ex01.java
@@ -7,6 +7,6 @@ import java.util.function.Consumer;
 public class Ex01 {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("이름1", "이름2", "이름3", "이름4");
-        names.forEach(s -> System.out.println(s));
+        //names.forEach(s -> System.out.println(s));
     }
 }
--- 4. 프로그래밍 언어활용/source/day14/src/exam01/Ex05.java
+++ 4. 프로그래밍 언어활용/source/day14/src/exam01/Ex05.java
@@ -7,7 +7,7 @@ public class Ex05 {
     public static void main(String[] args) {
         Function<String, Integer> func1 = s -> s.length(); //문자열길이
         Function<Integer, Integer> func2 = x -> x * x; //제곱
-        //IntFunction func2 = x -> x * x;
+
         Function<String, Integer> func3 = func1.andThen(func2); //함수결합(func1->func2 실행)
         Function<String, Integer> func4 = func2.compose(func1); //함수결합(func2->func1 실행)
 
