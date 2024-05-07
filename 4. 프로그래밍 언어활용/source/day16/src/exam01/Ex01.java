05d5105106945502fbb90a9253805d128a4bcb9d
hyeon12 <seojh912_@naver.com>
2024-05-07 14:36:03 +0900

    스트림의 최종연산

--- 4. 프로그래밍 언어활용/source/day16/src/exam01/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day16/src/exam01/Ex01.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 public class Ex01 {
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 7, 9, 11, 13};
-        boolean isOdd = Arrays.stream(nums).allMatch(x -> x % 2 == 1);
+        boolean isOdd = Arrays.stream(nums).allMatch(x -> x % 2 == 1);//모두 참일때
         System.out.println(isOdd);
 
-        boolean isEven = Arrays.stream(nums).anyMatch(x -> x % 2 == 0);
+        boolean isEven = Arrays.stream(nums).anyMatch(x -> x % 2 == 0);//하나라도 참일때
         System.out.println(isEven);
+
+        boolean notIncludedEven = Arrays.stream(nums).noneMatch(x -> x % 2 == 0);//전부 거짓일때
+        System.out.println(notIncludedEven);
         /*
         boolean result = true;
         for(int num : nums) {
