5fe55e6a2bc5f99c9d48a8a1f57451e84c9b9d64
hyeon12 <seojh912_@naver.com>
2024-05-08 22:33:29 +0900

    예제연습

---
+++ 4. 프로그래밍 언어활용/source/home/study01/src/Shopping/Ex.java
@@ -0,0 +1,16 @@
+package Shopping;
+
+public class Ex {
+    public static void main(String[] args) {
+        Order o1 = new Order();
+
+        o1.OrderNo="20240502001";
+        o1.id="a123";
+        o1.date="2024년 5월 2일";
+        o1.name="김누구";
+        o1.productNum="PD0502-12";
+        o1.address="서울시 마포구 신촌로 176";
+
+        System.out.println(o1);
+    }
+}
---
+++ 4. 프로그래밍 언어활용/source/home/study01/src/Shopping/Order.java
@@ -0,0 +1,20 @@
+package Shopping;
+
+public class Order {
+    String OrderNo;
+    String id;
+    String date;
+    String name;
+    String productNum;
+    String address;
+
+    @Override
+    public String toString() {
+        return  "OrderNo=" + OrderNo + '\n'+
+                "id=" + id + '\n' +
+                "date=" + date + '\n'+
+                "name=" + name + '\n' +
+                "productNum=" + productNum + '\n'+
+                "address=" + address;
+    }
+}
