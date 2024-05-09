b108d002867566d06c4fea549f9cfe2e7e3d606b
hyeon12 <seojh912_@naver.com>
2024-05-09 10:13:31 +0900

    예제연습:static과 인스턴스변수에 대한 이해

--- 4. 프로그래밍 언어활용/source/home/study01/src/student/Student.java
+++ 4. 프로그래밍 언어활용/source/home/study01/src/student/Student.java
@@ -2,16 +2,33 @@ package student;
 
 public class Student {
     private static int studentId;
+    private int _studentId;
     private int cardNum;
     String name;
 
     public Student(String name){
         this.name = name;
+        _studentId = ++studentId;
+        cardNum = _studentId + 100;
+    }
+
+    public String toString(){
+        return String.format("학생이름: %s, 학번: %d, 학생 카드 번호: %d%n", name, _studentId, cardNum);
+    }
+
+    /*
+    private static int studentId;
+    private int cardNum;
+    String name;
+
+    public Student(String name){
+        this.name = name;
+        //studentId++;
         this.cardNum = (studentId++) + 100;
     }
 
     public String toString(){
         return String.format("학생이름: %s, 학번: %d, 학생 카드 번호: %d%n", name, studentId, cardNum);
     }
-
+    */
 }
--- 4. 프로그래밍 언어활용/source/home/study01/src/student/StudentTest.java
+++ 4. 프로그래밍 언어활용/source/home/study01/src/student/StudentTest.java
@@ -4,8 +4,12 @@ public class StudentTest {
     public static void main(String[] args) {
         Student s1 = new Student("일이름");
         Student s2 = new Student("이이름");
+        Student s3 = new Student("삼이름");
+        Student s4 = new Student("사이름");
 
         System.out.println(s1);
         System.out.println(s2);//왜 학번 출력이 이상하게 되지?ㅜㅜ
+        System.out.println(s3);
+        System.out.println(s4);
     }
 }
