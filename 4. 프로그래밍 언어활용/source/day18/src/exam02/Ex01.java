574c0dbba38d7849c2a01499487b7de7e2f28793
hyeon12 <seojh912_@naver.com>
2024-05-09 15:47:25 +0900

    java.time 달력 만들기 과제 힌트

---
+++ 4. 프로그래밍 언어활용/source/day18/src/exam02/Ex01.java
@@ -0,0 +1,17 @@
+package exam02;
+
+import java.time.LocalDate;
+
+//매월 1일이 무슨 요일에 시작하는지?
+public class Ex01 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        LocalDate startDate = LocalDate.of(today.getYear(), today.getMonth(), 1);
+
+        int yoil = startDate.getDayOfWeek().getValue(); //1(월)~7(일)
+        System.out.println(yoil);//시작 요일 구하기!
+
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);//다음달-1일
+        System.out.println(endDate);//마지막 날짜를 구하기!
+    }
+}
