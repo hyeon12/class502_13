62e89f84fdeb807eda500ef41eae4221e6345f50
hyeon12 <seojh912_@naver.com>
2024-06-19 10:41:22 +0900

    회원가입/로그인 페이지 - 기본설정(DB 연결)

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/configs/DBConn.java
@@ -0,0 +1,36 @@
+package org.choongang.global.configs;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+//DB 설정파일
+public class DBConn {
+    private static SqlSessionFactory factory;
+
+    static{
+        try {
+            //지정된 경로에 있는 xml 파일 읽어들이는 Reader 객체
+            Reader reader = Resources.getResourceAsReader("org/choongang/global/configs/mybatis-config.xml");
+
+            factory = new SqlSessionFactoryBuilder().build(reader);
+            //SqlSessionFactoryBuilder - 데이터베이스와의 세션을 관리하며,
+            //MyBatis 에서 SQL 세션을 실행하고 데이터베이스와의 연결을 관리하는 핵심 객체
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static SqlSession getSession(boolean autoCommit){
+        return factory.openSession(autoCommit);
+    }
+
+    public static SqlSession sqlSession(){
+        return getSession(true);
+    }
+}
