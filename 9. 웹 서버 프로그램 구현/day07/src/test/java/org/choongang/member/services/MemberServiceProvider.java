93759d93e1872257577a71071f0e7cf7af5b8aa1
hyeon12 <seojh912_@naver.com>
2024-06-19 12:48:01 +0900

    객체 조립기(기능)

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/MemberServiceProvider.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/MemberServiceProvider.java
@@ -8,12 +8,13 @@ public class MemberServiceProvider {
     //싱글톤패턴 - 객체조립기=기능적인 내용으로 객체 만들 필요 없으므로!
     private static MemberServiceProvider instance;
 
-    private MemberServiceProvider(){}
+    private MemberServiceProvider() {}
 
-    private static MemberServiceProvider getInstance(){
-        if(instance == null){
+    public static MemberServiceProvider getInstance() {
+        if (instance == null) {
             instance = new MemberServiceProvider();
         }
+
         return instance;
     }
 
