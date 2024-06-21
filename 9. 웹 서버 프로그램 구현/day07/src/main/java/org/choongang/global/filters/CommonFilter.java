d87e6d03c80bb32d013865d5fbfa886f373bfaa2
hyeon12 <seojh912_@naver.com>
2024-06-21 09:54:34 +0900

    필터

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/filters/CommonFilter.java
@@ -0,0 +1,15 @@
+package org.choongang.global.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter("/*") //모든 페이지에 공통적으로 유입될 필터 /*
+public class CommonFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //모든 요청에 공통으로 응답할 필터
+        chain.doFilter(request, response);
+    }
+}
