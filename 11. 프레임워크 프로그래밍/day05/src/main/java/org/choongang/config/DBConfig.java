07e10feb57aa970e67ba1817edbf518d3be41beb
hyeon12 <seojh912_@naver.com>
2024-07-11 10:55:32 +0900

    DB 설정 클래스

---
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -0,0 +1,56 @@
+package org.choongang.config;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration //DB 설정 클래스
+@EnableTransactionManagement
+public class DBConfig {
+
+    @Bean(destroyMethod = "close") //스프링 컨테이너가 종료될 때, close() 메서드 호출
+    public DataSource dataSource(){//tomcat 쪽 DataSource
+        DataSource ds = new DataSource();
+
+        /* 연결 설정 S */
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        ds.setUsername("SPRING");
+        ds.setPassword("oracle");
+        /* 연결 설정 E */
+
+        /* 커넥션 풀 설정 S */
+        ds.setInitialSize(2);
+        ds.setMaxActive(10); // 최대 활성화 개수
+        ds.setTestWhileIdle(true); // 연결 유효성 검사
+        ds.setMinEvictableIdleTimeMillis(1000 * 60); // 기본 설정값 1분 (없으면 기본 값으로)
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 5); // 기본 설정값 5초 (없으면 기본 값으로)
+        /* 커넥션 풀 설정 E */
+
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory()throws Exception{
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        return factoryBean.getObject();//getObject() : SqlSessionFactory 객체 반환
+    }
+}
