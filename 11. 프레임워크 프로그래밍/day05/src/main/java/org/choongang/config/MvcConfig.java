c0c21b0ef7c857026a43f59b297114b23bf70a64
hyeon12 <seojh912_@naver.com>
2024-07-11 16:15:09 +0900

    스프링 MVC

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -4,10 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration //웹에 대한 설정 클래스
 @EnableWebMvc
@@ -21,6 +18,14 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //정적 경로 설정
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+
     }
 
     @Override
--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,13 +1,45 @@
 package org.choongang.member.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/member") //공통요청
 public class MemberController {
 
-    @GetMapping("/member/join")
+
+    //@RequestMapping(path = "/member/join", method = {RequestMethod.GET, RequestMethod.POST})
+    //GET, POST 모두 가능한 방식 (method 를 따로 설정하지 않으면, 모든 요청 메서드에 매핑 가능)
+    @GetMapping("/join")
     public String join(){
         return "member/join";
     }
+
+    @PostMapping("/join")
+    public String joinPs(RequestJoin form){
+        //POST -> 사용자가 넘긴 데이터 값을 활용하게 됨 -> 커맨드 객체
+        return "redirect:/member/login";
+        //contextPath 뒤에 붙어서, 주소 이동을 하는 것!
+        //templates 아님.
+    }
+
+    /*
+    @GetMapping("/member/join")
+    public ModelAndView join(){
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "Hello");
+        mv.setViewName("member/join");
+        //member/join 쪽을 버퍼에 추가하고 출력해준다..! (View - 템플릿 경로)
+
+        return mv;
+    }
+     */
 }
+
