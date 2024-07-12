9a06cdaaac19f4861f54d99c61a7639a5cfefa7b
hyeon12 <seojh912_@naver.com>
2024-07-12 16:10:14 +0900

    검증

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/validators/JoinValidator.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/validators/JoinValidator.java
@@ -4,25 +4,38 @@ package org.choongang.member.validators;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.exceptions.BadRequestException;
 import org.choongang.global.validators.RequiredValidator;
-import org.choongang.global.validators.Validator;
 import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.mappers.MemberMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 @Component
 @RequiredArgsConstructor
-public class JoinValidator implements Validator<RequestJoin>, RequiredValidator {
+public class JoinValidator implements Validator {
 
     private final MemberMapper mapper;
 
     @Override
+    public boolean supports(Class<?> clazz) {
+        // RequestJoin 커맨드 객체만 검증하도록 제한
+        return clazz.isAssignableFrom(RequestJoin.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+    }
+
+    /*
+    @Override
     public void check(RequestJoin form) {
-        /**
+
          * 1. 필수항목검증(email, password, confirmPassword, userName)
          * 2. 이메일 중복 여부(회원이 가입되어 있는지 체크) -> DB 정보 조회 -> 의존성 추가
          * 3. 비밀번호 자리수 체크(8자리)
          * 4. 비밀번호, 비밀번호 확인 일치 여부
-         */
+
 
         String email = form.getEmail();
         String password = form.getPassword();
@@ -46,4 +59,5 @@ public class JoinValidator implements Validator<RequestJoin>, RequiredValidator 
         //비밀번호 - 비밀번호 확인 일치 체크
         checkTrue(password.equals(confirmPassword), new BadRequestException("비밀번호가 일치하지 않습니다."));
     }
+    */
 }
