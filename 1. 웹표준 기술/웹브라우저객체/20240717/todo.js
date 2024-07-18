b519f70b1f24b722b29687e63f541b0f80490e2a
hyeon12 <seojh912_@naver.com>
2024-07-18 18:02:51 +0900

    이벤트

--- 1. 웹표준 기술/웹브라우저객체/20240717/todo.js
+++ 1. 웹표준 기술/웹브라우저객체/20240717/todo.js
@@ -1,9 +1,36 @@
 const todos = {
+    // 템플릿 가져오기
+    getTpl() {
+      const tplEl = document.getElementById("tpl");
+  
+      return tplEl.innerHTML;
+    },
     // 스케줄 추가
-    add(todo){
-        
-    }
-};
+    add(todo) {
+      todo = todo ?? "";
+      todo = todo.trim();
+      if (!todo) {
+        throw new Error("스케줄을 입력하세요.");
+      }
+  
+      let html = this.getTpl();
+      html = html.replace(/#todo#/g, todo);
+  
+      const domParser = new DOMParser();
+      const dom = domParser.parseFromString(html, "text/html");
+      const liEl = dom.querySelector("li");
+  
+      const schedulesEl = document.getElementById("schedules");
+      schedulesEl.append(liEl);
+  
+      const buttonEl = liEl.querySelector("button");
+      buttonEl.addEventListener("click", function () {
+        if (confirm("정말 삭제하겠습니까?")) {
+          liEl.remove();
+        }
+      });
+    },
+  };
 
 window.addEventListener("DOMContentLoaded", function(){
     //submit 이벤트가 발생했을 때~
@@ -15,6 +42,14 @@ window.addEventListener("DOMContentLoaded", function(){
         const todoEl = this.todo;
         //이벤트 발생한 요소 = this(frmTodo)
         //this.todo => frmTodo.todo (input)
-        console.log(todoEl.value);
+        try {
+            todos.add(todoEl.value);
+      
+            todoEl.value = "";
+        } catch (err) {
+        alert(err.message);
+        }
+    
+        todoEl.focus();
     });
 });
