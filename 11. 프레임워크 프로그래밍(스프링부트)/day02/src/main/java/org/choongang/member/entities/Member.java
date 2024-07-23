2425a6093b3bc73e8330f58dc3d90c9c3362c56b
hyeon12 <seojh912_@naver.com>
2024-07-23 16:02:22 +0900

    JPA 엔티티 매핑 관련 애노테이션

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
@@ -1,24 +1,36 @@
 package org.choongang.member.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.choongang.member.constants.Authority;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+/*
 @Table(indexes = {@Index(name="idx_created_at_desc", columnList="createdAt DESC"),
                  @Index(name="uq_email_password", columnList = "email, password", unique = true)})
-
+*/
 public class Member {
-    @Id // 기본 키 지정
+    @Id @GeneratedValue
     private Long seq;
     private String email;
     private String password;
     private String userName;
+
+    //@Lob
+    @Transient // 내부에서 2차 가공해서 넣어줄 데이터
+    private String introduction;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @CreationTimestamp
     private LocalDateTime createdAt; //생성 일자
+
+    @UpdateTimestamp
     private LocalDateTime modifiedAt; //수정 일자
 }
---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/test/java/org/choongang/jpa_study/Ex04.java
@@ -0,0 +1,44 @@
+package org.choongang.jpa_study;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.choongang.member.entities.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(properties = "spring.profiles.active=test")
+public class Ex04 {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    void test1() throws Exception {
+        Member member = new Member();
+        member.setEmail("user01@test.org");
+        member.setPassword("12345678");
+        member.setUserName("김사용");
+
+        em.persist(member);
+        em.flush();
+
+        em.clear();
+
+        member = em.find(Member.class, member.getSeq());
+        System.out.println(member);
+
+        Thread.sleep(5000);
+        member.setUserName("(수정)김사용");
+        em.flush();
+        em.clear();
+
+        member = em.find(Member.class, member.getSeq());
+        System.out.println(member);
+    }
+}
