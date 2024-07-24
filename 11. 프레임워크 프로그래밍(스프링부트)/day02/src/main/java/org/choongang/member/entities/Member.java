de721799b58664a2308f8aeae3d10feed807dc03
hyeon12 <seojh912_@naver.com>
2024-07-24 09:18:08 +0900

    JPA 엔티티 매핑 관련 애노테이션

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
@@ -10,12 +10,15 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
+//@Table(name="CH_MEMBER") // 테이블 이름이 클래스 명과 다른 경우
 /*
-@Table(indexes = {@Index(name="idx_created_at_desc", columnList="createdAt DESC"),
-                 @Index(name="uq_email_password", columnList = "email, password", unique = true)})
-*/
+@Table(indexes = {
+        @Index(name="idx_created_at_desc", columnList="createdAt DESC"),
+        @Index(name="uq_email_password", columnList = "email, password", unique = true)
+        })*/
+
 public class Member {
-    @Id @GeneratedValue
+    @Id /* @GeneratedValue(strategy=GenerationType.AUTO) */ @GeneratedValue
     private Long seq;
     private String email;
     private String password;
