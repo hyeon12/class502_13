4df50cfac1549c9de6073f356cd14696a1e90def
hyeon12 <seojh912_@naver.com>
2024-07-26 11:55:30 +0900

    @Scheduled

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/global/config/MvcConfig.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/global/config/MvcConfig.java
@@ -2,11 +2,13 @@ package org.choongang.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@EnableWebMvc -> 스프링 부트 내에서 자동 실행
+@EnableScheduling
 @EnableJpaAuditing
 @Configuration
-//@EnableWebMvc -> 스프링 부트 내에서 자동 실행
 public class MvcConfig implements WebMvcConfigurer {
 
 }
