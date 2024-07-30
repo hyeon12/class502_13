03ef97ab65d9353bf5125d51e0d184ecb65fbaca
hyeon12 <seojh912_@naver.com>
2024-07-30 10:23:38 +0900

    초기 설정

---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day90/src/main/java/org/hyeon/day90/Day90Application.java
@@ -0,0 +1,13 @@
+package org.hyeon.day90;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day90Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day90Application.class, args);
+	}
+
+}
---
+++ 11. 프레임워크 프로그래밍(스프링부트)/day90/src/test/java/org/hyeon/day90/Day90ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeon.day90;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day90ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
